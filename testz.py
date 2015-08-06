4dc04933ba430641d1729ce00a97783f34e48311
Blackbush <rommelrizzato@hotmail.com>
2015-08-06 03:57:48 -0700

    adding something else

--- testz.py
+++ testz.py
@@ -2,5 +2,18 @@
 
 
 #this is a test
-print 'Rommel you are Gorgeous !!!'
+print 'Hello There'
+
+dict={}
+my_list = range(10, 35)
+my_list.append('something')
+my_list.append(3)
+my_list.append(dict)
+print my_list
+
+print "\n\n"
+
+for i,j in enumerate(my_list):
+    print i,j
+
 
